bc442749639660944c287d75266be804803717a9
adelic-penguins <esperanto0616@gmail.com>
2025-06-09 22:44:29 +0900

    [WIP]コンポーネントファイル作成

--- app/types/userState/card.ts
+++ app/types/userState/card.ts
@@ -1,3 +1,19 @@
 export type SelectedCardState = {
-    [key: string]: boolean
+	[key: string]: boolean;
+};
+export enum phase {
+	// カード使用フェーズ
+	cardUsagePhase,
+	// 能力発動フェーズ
+	abilityPhase,
+	// お題入力フェーズ
+	themeInputPhase,
+	// 回答フェーズ
+	answerPhase,
+	// 評価フェーズ
+	evaluationPhase,
+	// 評価後の結果フェーズ
+	evaluationResultPhase,
+	// ゲーム終了フェーズ
+	gameEndPhase,
 }
