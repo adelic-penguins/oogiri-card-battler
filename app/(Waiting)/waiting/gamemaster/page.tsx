f91072937cfac233ef8bcdad1fcbb13a3becb1ec
Seiya Iwabuchi <0123ook@gmail.com>
2025-06-11 21:54:20 +0900

    大文字->小文字

---
+++ app/(Waiting)/waiting/gamemaster/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+import GameMaster from "@/app/feature/Waiting/gamemaster/GameMaster";
+
+const GameMasterPage: React.FC = () => {
+	return <GameMaster />;
+};
+export default GameMasterPage;
