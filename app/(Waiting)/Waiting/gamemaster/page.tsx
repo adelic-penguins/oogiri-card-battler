912a85fee9f66a53cab569ac92450e282ffd7688
adelic-penguins <esperanto0616@gmail.com>
2025-06-11 21:28:51 +0900

    大文字のディレクトリ削除

--- app/(Waiting)/Waiting/gamemaster/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React from "react";
-import GameMaster from "@/app/feature/Waiting/gamemaster/GameMaster";
-
-const GameMasterPage: React.FC = () => {
-	return <GameMaster />;
-};
-export default GameMasterPage;
--- app/(Waiting)/Waiting/player/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React from "react";
-import Player from "@/app/feature/Waiting/player/Player";
-
-const PlayerPage: React.FC = () => {
-	return <Player />;
-};
-export default PlayerPage;
