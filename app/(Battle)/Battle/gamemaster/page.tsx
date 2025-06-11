e04213b135b7d7ecb13dd085ff737960b2185337
Seiya Iwabuchi <0123ook@gmail.com>
2025-06-11 21:53:22 +0900

    大文字->小文字

--- app/(Battle)/Battle/gamemaster/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import type React from "react";
-import GameMaster from "@/app/feature/Battle/gamemaster/GameMaster";
-
-const GameMasterPage: React.FC = () => {
-	return <GameMaster />;
-};
-export default GameMasterPage;
--- app/(Battle)/Battle/player/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import type React from "react";
-import Player from "@/app/feature/Battle/player/Player";
-
-const PlayerPage: React.FC = () => {
-	return <Player />;
-};
-export default PlayerPage;
