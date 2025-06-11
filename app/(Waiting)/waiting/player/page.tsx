0a0aabc579ac8862802eab5ee23e4e60dba53d21
Seiya Iwabuchi <0123ook@gmail.com>
2025-06-11 21:10:49 +0900

    大文字->小文字

---
+++ app/(Waiting)/waiting/player/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+import Player from "@/app/feature/Waiting/player/Player";
+
+const PlayerPage: React.FC = () => {
+	return <Player />;
+};
+export default PlayerPage;
